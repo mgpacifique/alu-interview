c324cc7e19fe6edd6db8d8c7ff5e103a97e30c7e
mgpacifique <ovouzzy@gmail.com>
2026-05-14 20:05:25 +0200

    refactor minOperations function to improve operation calculation logic and enhance docstring clarity

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """Minimum Operations"""
 
+
 def minOperations(n):
     """Calculates the fewest number of operations
-    needed to result in exactly n H characters in the file."""
+    needed to result in exactly n H characters in the file.
+    """
     if n <= 1:
         return 0
 
     operations = 0
-    current_length = 1
+    divisor = 2
 
-    while current_length < n:
-        if n % current_length == 0:
-            operations += 2  # Copy All and Paste
-            current_length *= 2
-        else:
-            operations += 1  # Paste
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
 
     return operations
